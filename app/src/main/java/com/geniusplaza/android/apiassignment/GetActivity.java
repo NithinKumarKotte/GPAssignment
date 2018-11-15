0a955f42009afdedb48019b1c2b0b742e465f49e
NithinKumarKotte <nithinkumar11891@gmail.com>
2018-11-15 13:47:27 -0500

    Second Commit

--- app/src/main/java/com/geniusplaza/android/apiassignment/GetActivity.java
+++ app/src/main/java/com/geniusplaza/android/apiassignment/GetActivity.java
@@ -1,13 +1,117 @@
 package com.geniusplaza.android.apiassignment;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.geniusplaza.android.apiassignment.Data.UserList;
+import com.geniusplaza.android.apiassignment.Data.Users;
+import com.geniusplaza.android.apiassignment.adapter.UserAdapter;
+import com.geniusplaza.android.apiassignment.services.WebService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class GetActivity extends AppCompatActivity {
+
+    UserAdapter userAdapter;
+    RecyclerView recyclerView;
+    List<Users> listOfUsers = new ArrayList<>();
+    int totalNumUsers;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get);
+
+        recyclerView = findViewById(R.id.getlist);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        requestData();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+
+    private void requestData() {
+        final WebService webService = WebService.retrofit.create(WebService.class);
+        Call<Users> callCount = webService.countPages();
+
+        //Asynchronous call to retrieve total number pages and total number of users
+        callCount.enqueue(new Callback<Users>() {
+            @Override
+            public void onResponse(Call<Users> call, Response<Users> response) {
+                Log.i("hello world",String.valueOf(response.body().count));
+
+                //number of pages
+                int count =  response.body().count;
+
+                //total number of users in all the pages
+                totalNumUsers = response.body().totalUsers;
+                for(int i=1;i<=count;i++){
+                    Call<UserList> callPage = webService.userItems(i);
+                   sendRequest(callPage);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Users> call, Throwable t) {
+
+            }
+        });
+
+    }
+
+    private void sendRequest(Call<UserList> call) {
+        call.enqueue(new Callback<UserList>() {
+            @Override
+            public void onResponse(Call<UserList> call, Response<UserList> response) {
+                UserList dataItems = response.body();
+
+                Toast.makeText(GetActivity.this, "Received user details from service",
+                        Toast.LENGTH_SHORT).show();
+
+                for (Users users: dataItems.getUsers()) {
+                    System.out.println(users.getFirstName());
+
+                    //Arraylist to store all the users
+                    listOfUsers.add(users);
+                }
+
+                if(listOfUsers.size()==totalNumUsers){
+                    displayData();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserList> call, Throwable t) {
+                Log.i("On Failure", t.getLocalizedMessage());
+                Toast.makeText(GetActivity.this, "Error: Failed to receive items from service",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    private void displayData() {
+        if (listOfUsers != null) {
+                userAdapter = new UserAdapter(this, listOfUsers);
+                recyclerView.setAdapter(userAdapter);
+        }
     }
 }
--- app/src/main/java/com/geniusplaza/android/apiassignment/MainActivity.java
+++ app/src/main/java/com/geniusplaza/android/apiassignment/MainActivity.java
@@ -1,13 +1,40 @@
 package com.geniusplaza.android.apiassignment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView getApi;
+    TextView postApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getApi = findViewById(R.id.getAPI);
+        postApi = findViewById(R.id.postAPI);
+
+        getApi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,GetActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        postApi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,PostActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
